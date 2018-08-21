0d89ba1e1d534ddda79118ec1698ea220d52f9d2
Marcus Guimaraes <mzg218@nyu.edu>
2018-08-21 17:04:29 -0400

    Initial Commit

---
+++ vulkantest/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  vulkantest
+//
+//  Created by Marcus Guimaraes on 8/21/18.
+//  Copyright © 2018 Marcus Guimaraes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
