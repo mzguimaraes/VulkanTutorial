c2dd53423bd719120e9946665f18850cb18b936e
Marcus Guimaraes <mzg218@nyu.edu>
2018-08-22 19:49:20 -0400

    added framebuffers

--- vulkantest/main.cpp
+++ vulkantest/main.cpp
@@ -38,6 +38,10 @@
     a. create color buffer attachment
     b. define subpass(es)
     c. create pass
+ 
+ To draw:
+ 1. initialize framebuffers
+ 
  */
 
 
@@ -138,6 +142,7 @@ private:
     VkRenderPass renderPass;
     VkPipelineLayout pipelineLayout;
     VkPipeline graphicsPipeline;
+    std::vector<VkFramebuffer> swapchainFramebuffers;
     
     static std::vector<char> readFile(const std::string& filename) {
         std::ifstream file(filename, std::ios::ate | std::ios::binary);
@@ -176,6 +181,7 @@ private:
         
         createRenderPass();
         createGraphicsPipeline();
+        createFramebuffers();
     }
     
     void createGraphicsPipeline() {
@@ -355,6 +361,27 @@ private:
         
         if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass) != VK_SUCCESS) {
             throw std::runtime_error("Failed to create render pass");
+        }
+    }
+    
+    void createFramebuffers() {
+        swapchainFramebuffers.resize(swapchainImageViews.size());
+        
+        for (size_t i = 0; i < swapchainImageViews.size(); i ++) {
+            VkImageView attachments[] = { swapchainImageViews[i] };
+            
+            VkFramebufferCreateInfo framebufferInfo = {};
+            framebufferInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
+            framebufferInfo.renderPass = renderPass;
+            framebufferInfo.attachmentCount = 1;
+            framebufferInfo.pAttachments = attachments;
+            framebufferInfo.width = swapchainExtent.width;
+            framebufferInfo.height = swapchainExtent.height;
+            framebufferInfo.layers = 1;
+            
+            if (vkCreateFramebuffer(device, &framebufferInfo, nullptr, &swapchainFramebuffers[i]) != VK_SUCCESS) {
+                throw std::runtime_error("Failed to create framebuffer");
+            }
         }
     }
     
@@ -788,6 +815,10 @@ private:
     }
     
     void cleanup() {
+        for (auto framebuffer : swapchainFramebuffers) {
+            vkDestroyFramebuffer(device, framebuffer, nullptr);
+        }
+        
         vkDestroyPipeline(device, graphicsPipeline, nullptr);
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
         vkDestroyRenderPass(device, renderPass, nullptr);
