de452a5f5161f0810b991841059551bb4313b680
Marcus Guimaraes <mzg218@nyu.edu>
2018-08-22 16:01:39 -0400

    added shader stages

---
+++ vulkantest/shaders/compile.sh
@@ -0,0 +1,2 @@
+/Users/marcus/Desktop/Programming/vulkansdk-macos-1.1.82.1/macOS/bin/glslangValidator -V shader.vert
+/Users/marcus/Desktop/Programming/vulkansdk-macos-1.1.82.1/macOS/bin/glslangValidator -V shader.frag
---
+++ vulkantest/shaders/shader.cpp
@@ -0,0 +1,9 @@
+//
+//  shader.cpp
+//  vulkantest
+//
+//  Created by Marcus Guimaraes on 8/22/18.
+//  Copyright © 2018 Marcus Guimaraes. All rights reserved.
+//
+
+#include <stdio.h>
