c8957e7502f4b2eaff8d9b92b6e66aa8f1976983
Marcus Guimaraes <mzg218@nyu.edu>
2018-08-22 18:42:50 -0400

    render pass done

--- vulkantest/main.cpp
+++ vulkantest/main.cpp
@@ -34,6 +34,10 @@
  4. setup viewport, scissor for viewport state
  5. config rasterizer, multisampling, color blending
  6. config pipeline layout
+ 7. create render pass:
+    a. create color buffer attachment
+    b. define subpass(es)
+    c. create pass
  */
 
 
@@ -131,6 +135,7 @@ private:
     VkFormat swapchainImageFormat;
     VkExtent2D swapchainExtent;
     std::vector<VkImageView> swapchainImageViews;
+    VkRenderPass renderPass;
     VkPipelineLayout pipelineLayout;
     
     static std::vector<char> readFile(const std::string& filename) {
@@ -294,7 +299,38 @@ private:
     }
     
     void createRenderPass() {
+        VkAttachmentDescription colorAttachment = {};
+        colorAttachment.format = swapchainImageFormat;
+        colorAttachment.samples = VK_SAMPLE_COUNT_1_BIT;
         
+        colorAttachment.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
+        colorAttachment.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
+        
+        colorAttachment.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
+        colorAttachment.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
+        
+        colorAttachment.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
+        colorAttachment.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
+        
+        VkAttachmentReference colorAttachmentRef = {};
+        colorAttachmentRef.attachment = 0;
+        colorAttachmentRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+        
+        VkSubpassDescription subpass = {};
+        subpass.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
+        subpass.colorAttachmentCount = 1;
+        subpass.pColorAttachments = &colorAttachmentRef;
+        
+        VkRenderPassCreateInfo renderPassInfo = {};
+        renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
+        renderPassInfo.attachmentCount = 1;
+        renderPassInfo.pAttachments = &colorAttachment;
+        renderPassInfo.subpassCount = 1;
+        renderPassInfo.pSubpasses = &subpass;
+        
+        if (vkCreateRenderPass(device, &renderPassInfo, nullptr, &renderPass) != VK_SUCCESS) {
+            throw std::runtime_error("Failed to create render pass");
+        }
     }
     
     VkShaderModule createShaderModule(const std::vector<char>& code) {
@@ -728,6 +764,7 @@ private:
     
     void cleanup() {
         vkDestroyPipelineLayout(device, pipelineLayout, nullptr);
+        vkDestroyRenderPass(device, renderPass, nullptr);
         
         for (auto imageView : swapchainImageViews) {
             vkDestroyImageView(device, imageView, nullptr);
